bc31f294fd062ef4bde7c678ce7ac292c7e59d3c
Your Name <adamin@xbta.tk>
2024-02-11 19:06:46 +0800

    MT管理器_合集

---
+++ quick/NEW.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+sed -i '$d' all.json
+sed -i '$d' all.json
+sed -i '$d' all.json
+sed "s/名称/$1/;s/软件/$2/" 0.json >> all.json
+sed "s/名称/$1/g" 1.json > "$1.json"
